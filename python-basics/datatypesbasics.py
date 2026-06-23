92c5edd13e7624fca93d6a0d4bd0fcd7c4730399
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-23 18:36:10 +0530

    Created data types file

---
+++ python-basics/datatypesbasics.py
@@ -0,0 +1,22 @@
+# Data types - A data type in Python defines the kind of value a variable can hold and determines what operations can be performed on it.
+
+#int
+age = 18
+gpa = 8
+language = 5
+print (f"I'm {age} years old , My gpa is {gpa} , I know {language} languages")
+
+#float
+average = 90.5
+print (f"My average is {average}")
+
+#String
+name = "Renga Nayaki"
+print (name)
+
+#Boolean 
+IsForSale = True
+if IsForSale:
+    print ("Available")
+else:
+    print("Not Available")
