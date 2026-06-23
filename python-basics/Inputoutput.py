d74f432e37ef671385f8b3bac2d057be4c417fa3
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-24 05:17:33 +0530

    Created i\o file

---
+++ python-basics/Inputoutput.py
@@ -0,0 +1,7 @@
+#input - The input() function in Python is used to capture user input from the keyboard.
+
+Name = input ("What is ur name?");
+
+#output-print() for displaying output.
+
+print(f"Hello!{Name}")
