cd0fb2bff729d9aeb53cc105ebccba88a35b0328
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-24 08:32:30 +0530

    Update Conditionstatements.py

--- python-basics/Conditionstatements.py
+++ python-basics/Conditionstatements.py
@@ -1,1 +1,20 @@
 
+# if statements - iif the if black is true the statement written on it will be executed. or the else block will be executed.
+
+age  = input (what's your age)
+if (age<=18)
+              print ("you are eligible for voting")
+else 
+              print ("you are NOT eligible for voting")
+
+#elif statement - If more than two conditions occurs we use elif        
+
+
+age  = int (input ("what's your age? "))
+if(age<0) :
+              print ("You haven't been born yet")
+elif (age>=18):
+              print ("you are eligible for voting") 
+else:
+              print ("you are NOT eligible for voting")
+
