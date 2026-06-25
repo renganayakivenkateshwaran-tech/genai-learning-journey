65ca84ceb787c0f890638c6c236b4604ff66b519
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-25 12:21:01 +0530

    created python basics

---
+++ python-basics/mathfunction.py
@@ -0,0 +1,15 @@
+x=4.65
+y=8
+z=9
+result = print(round(x))
+result = print(abs(x))
+result = print(min(x,y,z))
+result = print(max(x,y,z))
+
+import math
+result = print(math.ceil(x))
+result = print(math.floor(x))
+result = print(round(math.sqrt(x) ,2))
+result = print(round(math.pow(x,y) ,2))
+result = print((math.pi))
+result = print((math.e))
---
+++ python-basics/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+x=4.65
+y=8
+z=9
+result = print(round(x))
+result = print(min(x,y,z))
