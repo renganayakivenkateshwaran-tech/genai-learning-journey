f7822f7ca85abbc6b6d19692c69effa7f3889964
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-26 17:31:47 +0530

    updated  while loop file

--- python-basics/while.py
+++ python-basics/while.py
@@ -6,7 +6,20 @@ while num<=10:
     num=num+1
 '''
 #2
+'''
 num=1
 while num<=10:
     print(f"3x{num}={num*3}")
     num=num+1
+'''
+#3 
+'''
+password="python123"
+user_pwd=input("Enter the password: ")
+
+while not user_pwd == "python123":
+    print("Incorrect password!")
+    user_pwd=input("Enter the correct password: ")
+
+print("Welcome!")
+'''
