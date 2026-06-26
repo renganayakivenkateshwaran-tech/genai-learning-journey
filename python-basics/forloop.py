e2f73c15d1e16f8d684509c3e6beb2f6585384d2
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-26 21:38:41 +0530

    Created for loop  file

---
+++ python-basics/forloop.py
@@ -0,0 +1,37 @@
+#1
+'''
+import time
+for x in range (0,11):
+    print(x)
+    time.sleep(1)
+    '''
+#2
+'''
+import time
+for x in reversed(range (0,11)):
+    print(x)
+    time.sleep(1)
+'''
+#3
+'''
+for x in reversed(range (0,11)):
+    if x==9:
+        continue
+    else:
+     print(x)
+'''
+#4
+'''
+for x in reversed(range (0,11)):
+    if x==9:
+        break
+    else:
+     print(x)
+'''
+#5
+'''
+credit_num= "7472-8362-9872"
+for x in credit_num:
+    print(x)
+'''
+
