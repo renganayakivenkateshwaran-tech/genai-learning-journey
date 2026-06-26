aead1a8d8e31a930ae4ea68b8951fe6bc77d2714
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-26 11:04:37 +0530

    Created string index file

---
+++ python-basics/stringindexing.py
@@ -0,0 +1,17 @@
+# String Indexing- You can access characters using square brackets []
+# Syntax - [Start:End:step]
+phone_number = "7364-37847-49834"
+print(phone_number[0])
+print(phone_number[0:5])
+print(phone_number[-1])
+print(phone_number[::2])
+print(phone_number[::-1])
+print(phone_number[5:9])
+print(phone_number[-4:])
+
+# Program to get the last 4 digits of the phone number
+phone_number = "7364-3787-49834"
+print(f"The last 4 digits are: XXXX-XXXX-{phone_number[-4:]}")
+
+
+
