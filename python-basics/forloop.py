56d75987967f78449db6b55ce5e80a083e4efe95
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-26 21:40:18 +0530

    updated for loop  file

--- python-basics/forloop.py
+++ python-basics/forloop.py
@@ -1,18 +1,18 @@
-#1
+#1-sleep function
 '''
 import time
 for x in range (0,11):
     print(x)
     time.sleep(1)
     '''
-#2
+#2-reverse keyword
 '''
 import time
 for x in reversed(range (0,11)):
     print(x)
     time.sleep(1)
 '''
-#3
+#3-continue
 '''
 for x in reversed(range (0,11)):
     if x==9:
@@ -20,7 +20,7 @@ for x in reversed(range (0,11)):
     else:
      print(x)
 '''
-#4
+#4-break
 '''
 for x in reversed(range (0,11)):
     if x==9:
@@ -28,7 +28,7 @@ for x in reversed(range (0,11)):
     else:
      print(x)
 '''
-#5
+#5-string range
 '''
 credit_num= "7472-8362-9872"
 for x in credit_num:
