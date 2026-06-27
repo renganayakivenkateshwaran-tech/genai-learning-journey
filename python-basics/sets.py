9f1533c3ee3f4024f714078ca22d1334bb90bd08
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-27 11:23:24 +0530

    Created sets file

--- python-basics/sets.py
+++ python-basics/sets.py
@@ -1,1 +1,13 @@
-stu_mark = {90,82,91,72,66}
+#Sets- Sets are unordered,unique(No duplicates),mutable,can't access by the index.
+stu1_mark = {90,82,91,72,66}
+stu2_mark = {72,89,92,74,62}
+
+stu1_mark.add(99)
+stu1_mark.remove(90)
+print(stu1_mark)
+print(stu2_mark)
+print(stu1_mark | stu2_mark)
+print(stu1_mark & stu2_mark)
+print(stu1_mark - stu2_mark)
+print(stu1_mark ^ stu2_mark)
+
