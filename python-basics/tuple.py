d8121d51ba6953a5377d681ddce7788990ed6152
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-27 11:32:08 +0530

    Created tuples file

---
+++ python-basics/tuple.py
@@ -0,0 +1,6 @@
+#Tuple- immutable,ordered,can have duplicates.
+fruits=("Apple","Orange","Berry","Coconut")
+print(fruits.index("Berry"))
+print(fruits.count("Apple"))
+print("Berry" in fruits)
+print(fruits)
