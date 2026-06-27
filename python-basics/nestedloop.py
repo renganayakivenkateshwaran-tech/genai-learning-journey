9c3444d2426d2707ee30f035ce013d1ee8415fae
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-27 10:44:06 +0530

    Created nested loop file

---
+++ python-basics/nestedloop.py
@@ -0,0 +1,11 @@
+#1
+'''
+for y in range(0,2):
+ for x in range(0,5):
+  print(x,end=",")
+  '''
+#2
+for y in range(4):
+ for x in range(2):
+  print("*")
+  print()
---
+++ python-basics/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+#1
+'''
+for y in range(0,2):
+ for x in range(0,5):
+  print(x,end=",")
