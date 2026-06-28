575a15ad885eb29989c23a2ed6c697e2ec969615
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-28 20:59:58 +0530

    Updated 2d  file

--- python-basics/2d.py
+++ python-basics/2d.py
@@ -2,6 +2,9 @@ fruits=["apple","orange","banana"]
 vegetables=["carrot","onion","beans"]
 junk=["burger","pizza","fries"]
 foods=[fruits,vegetables,junk]
+fruits.append("Mango")
+vegetables.count("onion")
+junk.sort()
 for food in foods:
  for col in food:
     print(col, end=" ")
