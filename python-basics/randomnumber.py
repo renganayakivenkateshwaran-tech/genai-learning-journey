68ca5f623ab02dc19d5aed5e16e089c8ca3bcd55
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-06-30 20:42:02 +0530

    Created Random numbers  file

---
+++ python-basics/randomnumber.py
@@ -0,0 +1,9 @@
+import random
+#print(random.randint(1,10)) #Random number between 1-10
+#print(random.randint(1,20)) #Random number between 1-20
+#print(random.random())      #float
+#option=("Rock","Paper","Scissors")
+#print(random.choice(option))
+options=["1","2","3","4","5","6","7","8"]
+(random.shuffle(options))
+print(options)
