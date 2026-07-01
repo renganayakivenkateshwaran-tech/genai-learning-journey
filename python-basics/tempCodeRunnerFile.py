9c00ad6304de54ec6a5e963adf23ebcce564f70c
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-07-01 08:05:11 +0530

    Delete python-basics/tempCodeRunnerFile.py

--- python-basics/tempCodeRunnerFile.py
+++
@@ -1,7 +0,0 @@
-#a dictionary is a built-in data type that stores data in key-value pairs.
-#It is ordered,mutable, and does not allow duplicate keys.
-students = {"BALA": "90",
-            "MEENA":"66",
-            "ACHU":"92"}
-#(students.get("ACHU"))
-#print(students.update({"PRIYA":"81"}))
