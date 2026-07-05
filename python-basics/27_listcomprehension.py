b730bf59cbd09cf3f9c04f66a6bc8ab10a09bf7b
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-07-05 12:22:13 +0530

    Created list comprehension file

---
+++ python-basics/27_listcomprehension.py
@@ -0,0 +1,2 @@
+#List comprehension is a concise and readable way to create lists from existing iterables by applying an expression and optionally filtering elements.
+#Syntax-[expression for value in range if condition]
