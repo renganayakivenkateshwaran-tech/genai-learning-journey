b77f7f19998045b2b20fcb0016b465717fb116c5
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-07-05 12:27:29 +0530

    Modified list comprehension file

--- python-basics/27_listcomprehension.py
+++ python-basics/27_listcomprehension.py
@@ -1,2 +1,8 @@
 #List comprehension is a concise and readable way to create lists from existing iterables by applying an expression and optionally filtering elements.
 #Syntax-[expression for value in range if condition]
+
+#1
+doubles = [x*2 for x in range(1,11)]
+print(doubles)
+five_table = [x*5 for x in range(1,11)]
+print(five_table)
