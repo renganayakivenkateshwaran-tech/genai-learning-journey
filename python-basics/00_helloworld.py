3d94d099aba7862a1b6f6cecae9c569c67977dc2
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-07-08 21:01:35 +0530

    Enhance hello world script with additional print examples
    Added examples of printing a number and a variable.

--- python-basics/00_helloworld.py
+++ python-basics/00_helloworld.py
@@ -1,2 +1,5 @@
 #The print() statement in Python is a built-in function used to display output on the screen (console).
-print ("Hello world!:)") 
+print ("Hello world!:)") #Printing string
+print (027) #Printing number
+name = achu
+print (name) #Printing variable
