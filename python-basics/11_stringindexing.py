c37e0d6452bd5253f7634a60873363571d8b1fa7
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-07-11 21:10:02 +0530

    Update string indexing examples in 11_stringindexing.py
    Comment out examples of string indexing and add a new phone number.

--- python-basics/11_stringindexing.py
+++ python-basics/11_stringindexing.py
@@ -1,13 +1,14 @@
 # String Indexing- You can access characters using square brackets []
 # Syntax - [Start:End:step]
 phone_number = "7364-37847-49834"
-#print(phone_number[0])
-#print(phone_number[0:5])
-#print(phone_number[-1])
-#print(phone_number[::2])
+#print(phone_number[0])     # 7
+#print(phone_number[0:5])   # 7364-
+#print(phone_number[-1])    # 4
+#print(phone_number[::2])  
 #print(phone_number[::-1])
 #print(phone_number[5:9])
 #print(phone_number[-4:])
+#print(phone_number[20])    # Index error  
 
 # Program to get the last 4 digits of the phone number
 phone_number = "7364-3787-49834"
