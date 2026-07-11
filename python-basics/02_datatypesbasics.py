5d3c676e30e24c01080dc1605a569529abe94298
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-07-11 21:20:36 +0530

    Refactor comment on data types for clarity
    Split the comment on data types into two lines for better readability.

--- python-basics/02_datatypesbasics.py
+++ python-basics/02_datatypesbasics.py
@@ -1,17 +1,21 @@
-# Data types - A data type in Python defines the kind of value a variable can hold and determines what operations can be performed on it.
+# Data types - A data type in Python defines the kind of
+# value a variable can hold and determines what operations can be performed on it.
 
 #int
 age = 18
 gpa = 8
 language = 5
+
 print (f"I'm {age} years old , My gpa is {gpa} , I speak {language} languages")
 
 #float
 average = 90.5
+
 print (f"My average is {average}")
 
 #String
 name = "Renga Nayaki"
+
 print (name)
 
 #Boolean 
