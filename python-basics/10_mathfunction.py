f7f6a22f0391c8f408e49b882355f4995442e03d
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-07-11 21:06:41 +0530

    Update math function examples in 10_mathfunction.py
    Uncomment math function examples and add comments for clarity.

--- python-basics/10_mathfunction.py
+++ python-basics/10_mathfunction.py
@@ -1,15 +1,18 @@
 x=4.65
 y=8
 z=9
-#result = print(round(x))
+#result = print(round(x))   # 4
 #result = print(abs(x))
-#result = print(min(x,y,z))
-#result = print(max(x,y,z))
+#result = print(min(x,y,z)) # x
+#result = print(max(x,y,z)) # z
 
 import math
 #result = print(math.ceil(x))
 #result = print(math.floor(x))
 #result = print(round(math.sqrt(x) ,2))
 #result = print(round(math.pow(x,y) ,2))
-#result = print((math.pi))
-#result = print((math.e))
+#print(math.pi)   # 3.141592653589793
+#print(math.e)    # 2.718281828459045
+#print(math.tau)  # 6.283185307179586 (2π)
+#print(math.inf)  # Infinity
+#print(math.nan)  # Not a Number
