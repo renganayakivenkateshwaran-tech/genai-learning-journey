52e35d1168c3649af8dbb3b6f52962679a4cda2c
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-08-23 17:56:31 +0530

    Created function files with parameters,argumnets,return statement,Scope

--- python-basics/functions/function.py
+++ python-basics/functions/function.py
@@ -34,6 +34,36 @@ def return_demo(a,b,c,d):
 result=return_demo(4,5,2,4) 
 print(result)
 
+"""
+Scope - Scope defines where a variable is accessible in your code.
+
+"""
+# Local variable - A variable created inside a function
+
+def greet():
+    name="Alice"
+    print (f"Hello! {name}")
+greet()
+
+# Global variable - A variable created outside all functions.
+
+greet = "Hello"
+
+def greeting(name):
+    print(greet,name)
+greeting("Tom")
+
+# Global keyword - If we want to make changes in global variable we use global keyword.
+
+num = 5
+
+def add():
+    global num
+    return num+5
+print(add())
+
+ 
+
 
 
      
