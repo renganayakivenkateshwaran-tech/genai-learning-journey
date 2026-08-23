de7b6a3d395763f4524acecb91991c677e4c2a0b
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-08-23 18:13:51 +0530

    Created advanced function concepts (Unpacking)

--- python-basics/functions/advanced_funtion_concepts.py
+++ python-basics/functions/advanced_funtion_concepts.py
@@ -17,3 +17,16 @@ def get_details():
     country = "America"
     return name,age,country
 print(get_details())
+
+# Unpacking returned values - A function can return multiple values by packing them into a tuple (or other iterable), and you can unpack them directly into variables.
+
+def get_details():
+    name = "Alice"
+    age = 20
+    country = "America"
+    return name,age,country
+name,age,country = get_details()
+print(name)
+print(age)
+print(country)
+
