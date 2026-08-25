3b5df281ed38bb34df342ef12f75882f08dbd9a9
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-08-25 08:09:59 +0530

    Add map and filter examples to functional_programming.py
    Added examples for map and filter functions in Python.

--- python-basics/functions/functional_programming.py
+++ python-basics/functions/functional_programming.py
@@ -10,3 +10,15 @@ Concept Covered
 - any()
 - all()
 """
+
+#map() - map() applies a function to every item in an iterable.
+numbers = [1,2,3,4,5,6,7,8]
+
+res = list(map(lambda num : num + num , numbers))
+print(res)
+
+#filter() - filter() function extracts elements from an iterable that satisfy a specific condition.
+numbers = [1,2,,3,7,9,5,3]
+
+res = list(filter(lambda num : num % 2 == 0,numbers))
+print(res)
