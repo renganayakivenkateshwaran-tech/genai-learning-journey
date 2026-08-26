cb914116bc39f8fc3947a9f071532dd846c3b511
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-08-26 16:26:44 +0530

    Added docstrings to the file

---
+++ python-basics/functions/recursion.py
@@ -0,0 +1,18 @@
+"""
+Demonstration of recursion in python.
+
+Concept covered
+- Recursion structure
+- Base case
+- Recursive case
+- Example programs
+"""
+
+"""
+Recursion is when a function calls itself directly or indirectly to solve a problem by breaking it into smaller subproblems.
+A recursive function must have:
+- Base Case  -  Stops the recursion.
+- Recursive Case  - Calls itself with a smaller/simpler input.
+"""
+# sum of n numbers
+
