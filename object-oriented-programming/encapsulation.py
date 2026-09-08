552f5dda6bfbca43eda1eb31867a8e0a3396a02a
renganayakivenkateshwaran-tech <renganayaki2008@gmail.com>
2026-09-08 07:16:27 +0530

    Fix typos and enhance comments in encapsulation.py
    Corrected typos and improved formatting in comments and docstrings.

--- object-oriented-programming/encapsulation.py
+++ object-oriented-programming/encapsulation.py
@@ -1,82 +1,117 @@
 """
-Deomstration of encapsulation.
+Demonstration of encapsulation.
 
 Concepts Covered:
-- Public attributes.
-- Protected attributes.
-- Private attributes.
+- Public attributes
+- Protected attributes
+- Private attributes
 - Name mangling
 - @property
 - Getter
 - Setter
 """
+
 """
-Encapsulation -  The practice of binding data and methods into a single class and restricting direct access to some attributes 
-                                        to protect the internal state of an object.
+Encapsulation - The practice of binding data and methods into a single class
+and restricting direct access to some attributes to protect the internal state
+of an object.
 """
+
+
 # Public attributes - Can be accessed directly from outside the class.
 class Student:
-    def __init__(self,name,age):
+    def __init__(self, name, age):
         self.name = name
-        self.age = age 
-student1 = Student("Ani",18)
-print(student1.name)
-print(student1.age)
+        self.age = age
 
-# Protected attributes - Is a class member (variable or method) whose name starts with a single underscore (_).
+
+# Protected attributes - A class member whose name starts with a single
+# underscore (_).
 class Employee:
-    def __init__(self,name,id_no,salary):
+    def __init__(self, name, id_no, salary):
         self.name = name
         self.id_no = id_no
         self._salary = salary
+
     def show_salary(self):
         print(self._salary)
-employee = Employee("Akilan",101,50000)
-employee.show_salary()
 
-# Private attributes - Indicates that an attribute is intended to be private using double underscore.
+
+# Private attributes - Indicates that an attribute is intended to be private
+# using double underscore.
 class BankAccountBasic:
-    def __init__(self,balance):
+    def __init__(self, balance):
         self.__balance = balance
-    def show_balance(self):
-        print("Balance:",self.__balance)
-account1 = BankAccountBasic(100000)
-account1.show_balance()
 
-# Name mangling - Used to make class attributes less accessible from outside the class and to avoid accidental name conflicts, especially in inheritance.
+    def show_balance(self):
+        print("Balance:", self.__balance)
+
+
+# Name mangling - Used to make class attributes less accessible from outside
+# the class and to avoid accidental name conflicts, especially in inheritance.
 class Code:
     def __init__(self):
         self.__code = "apple123"
-obj = Code()
-print(obj._Code__code)
 
-# @property Makes mathods behave like attributes.
 
+# @property - Makes methods behave like attributes.
 class Add:
-    def __init__(self,num):
+    def __init__(self, num):
         self.__num = num
+
     @property
     def addition(self):
         return self.__num + 90
-num1 = Add(10)
-print(num1.addition)
+
 
 # Getters - Used to retrieve internal data.
 # Setters - Used to change the internal data.
-
 class BankAccountWithProperty:
-    def __init__(self,balance):
+    def __init__(self, balance):
         self.__balance = balance
+
     @property
     def balance(self):
         return self.__balance
-    @balance.setter   
-    def balance(self,amount):
+
+    @balance.setter
+    def balance(self, amount):
         if amount > 0:
-             self.__balance = self.__balance + amount
+            self.__balance = self.__balance + amount
         else:
             print("Deposit number can't be negative")
-account2 = BankAccountWithProperty(10000)
-print(account2.balance)
-account2.balance = 5000
-print(account2.balance)
+
+
+# --------------------------------------------------
+# Demo
+# --------------------------------------------------
+
+if __name__ == "__main__":
+
+    # Public attributes
+    student1 = Student("Ani", 18)
+    print(student1.name)
+    print(student1.age)
+
+    # Protected attributes
+    employee = Employee("Akilan", 101, 50000)
+    employee.show_salary()
+
+    # Private attributes
+    account1 = BankAccountBasic(100000)
+    account1.show_balance()
+
+    # Name mangling
+    obj = Code()
+    print(obj._Code__code)
+
+    # @property
+    num1 = Add(10)
+    print(num1.addition)
+
+    # Getter and Setter
+    account2 = BankAccountWithProperty(10000)
+    print(account2.balance)
+
+    account2.balance = 5000
+    print(account2.balance)
